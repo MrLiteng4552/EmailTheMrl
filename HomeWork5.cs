a901a1d7ba54e86809824e27c3a80ebf3f939500
MrLiteng4552 <randoomchik6@gmail.com>
2025-11-02 11:22:27 +0300

    Add files via upload

---
+++ HomeWork5.cs
@@ -0,0 +1,37 @@
+﻿class FactorialCalculator
+{
+    static void Main()
+    {
+        Console.WriteLine("--- Калькулятор факториала ---");
+        Console.Write("Введите число: ");
+
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            if (n >= 0)
+            {
+                int factorial = CalculateFactorial(n);
+                Console.WriteLine($"Факториал числа {n} равен {factorial}");
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка: введите положительное число.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: введите целое число.");
+        }
+    }
+
+    static int CalculateFactorial(int n)
+    {
+        int result = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
+}
